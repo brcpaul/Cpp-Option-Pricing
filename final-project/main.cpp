f27ce56f29cb65a35bb93bfd38307c94569e6652
paulbourcereau <paul.bourcereau@icloud.com>
2023-10-25 16:59:14 +0200

    base files

---
+++ final-project/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  final-project
+//
+//  Created by Paul Bourcereau on 25/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+ 
