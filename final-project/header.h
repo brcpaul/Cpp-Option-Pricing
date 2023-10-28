6537b21f8db9e9fc5f83213cbef036f05dd10521
Antoine BUFFANDEAU <buffandeau.a@gmail.com>
2023-10-28 18:37:44 +0200

    modif header.h pour binarytree + une ligne dans main.cpp

--- final-project/header.h
+++ final-project/header.h
@@ -1,2 +1,17 @@
 #pragma once
+#include <iostream>
+#include <math.h>
+#include <vector>
 
+template <typename T>
+class BinaryTree {
+	private :
+		int _depth; //represents N
+		std::vector<std::vector<T>> _tree; //hold data of type T
+	public :
+		BinaryTree(int d) : _depth{d} {/*à finir*/ }
+		void setDepth(int d);
+		void setNode(int, int, T);
+		T getNode(int, int);
+		void display();
+};
--- final-project/main.cpp
+++ final-project/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include "header.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
