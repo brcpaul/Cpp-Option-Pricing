19e411e2502aca1516947e3c0a55e7c1e9c84615
paulbourcereau <paul.bourcereau@icloud.com>
2023-10-31 11:48:01 +0100

    Binary tree h cpp split

--- final-project/BinaryTree.cpp
+++ final-project/BinaryTree.cpp
@@ -1,1 +1,11 @@
 #include "BinaryTree.h"
+#include <iostream>
+
+void setDepth(int d){
+    _depth = d; //affectation de la profondeur
+    _tree.resize(d+1); //changement de la taille du vecteur (va de 0 ‡ d inclus)
+    for (int i = 0;i <= d;i++) {
+        _tree[i].resize(i + 1); //changement de la taille de chaque sous-vecteur du vecteur
+    }
+}
+
