baa5d3e6b61baa36f3473b26f22642a4243277da
Antoine BUFFANDEAU <buffandeau.a@gmail.com>
2023-10-31 20:14:13 +0100

    commit test 2 pour binarytree.cpp

--- final-project/BinaryTree.cpp
+++ final-project/BinaryTree.cpp
@@ -1,11 +1,12 @@
-#include "BinaryTree.h"
+#include "header.h"
 #include <iostream>
 
-void setDepth(int d){
-    _depth = d; //affectation de la profondeur
-    _tree.resize(d+1); //changement de la taille du vecteur (va de 0 ‡ d inclus)
-    for (int i = 0;i <= d;i++) {
-        _tree[i].resize(i + 1); //changement de la taille de chaque sous-vecteur du vecteur
-    }
-}
+//template <typename T>
+//void BinaryTree<T>::setDepth(int d){
+//    _depth = d; //affectation de la profondeur
+//    _tree.resize(d+1); //changement de la taille du vecteur (va de 0 ‡ d inclus)
+//    for (int i = 0;i <= d;i++) {
+//        _tree[i].resize(i + 1); //changement de la taille de chaque sous-vecteur du vecteur
+//    }
+//}
 
