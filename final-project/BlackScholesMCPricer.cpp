2a97650c2e283d94ab60ec82c21201286fb2e57b
user <emmaaoui24@gmail.com>
2023-11-22 10:27:23 +0100

    modif cpp/header emma

--- final-project/BlackScholesMCPricer.cpp
+++ final-project/BlackScholesMCPricer.cpp
@@ -1,2 +1,32 @@
 #include "BlackScholesMCPricer.h"
 
+void BlackScholesMCPricer::generate(int nb_paths)
+{// Méthode pour générer des chemins
+    for (int i = 0; i < nb_paths; ++i) {
+        double path = initialPrice;
+
+        // Formule de Black-Scholes pour générer des chemins
+        double drift = exp((interestRate - 0.5 * volatility * volatility) * 1.0);
+        double diffusion = exp(volatility * sqrt(1.0) * MT::rand_norm());
+
+        path = path * drift * diffusion;
+
+        // Mettre à jour le nombre de chemins générés
+        numberPaths++;
+
+        //UPDATES the current estimate of the price of the option
+        //méthode existante dans la classe BS Pricer ? TEST
+        double optionPrice; //= option->computeOptionPrice(path);
+        currentEstimate = ((currentEstimate * numberPaths) + optionPrice) / (numberPaths + 1);
+    }
+}
+
+double BlackScholesMCPricer::operator()()
+{
+    if (numberPaths == 0) {
+        throw std::runtime_error("Current estimate is undefined. No paths have been generated.");
+    }
+    return currentEstimate;
+}
+
+
--- final-project/MT.cpp
+++ final-project/MT.cpp
@@ -1,1 +1,15 @@
 #include "MT.h"
+
+double MT::rand_unif()
+{
+	std::uniform_real_distribution<double> dist(0.0, 1.0);
+	return dist(getInstance().rng);
+}
+
+double MT::rand_norm()
+{
+	std::normal_distribution<double> dist(0.0, 1.0);
+	return dist(getInstance().rng);
+}
+
+
